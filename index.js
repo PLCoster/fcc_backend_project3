d338a49ca02c9cc1cee03682de46b484f5d2f645
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-25 11:50:24 -0700

    Refactored /api/shorturl route into several middeware functions

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // .env file can hold PORT variable if desired
 require('dotenv').config();
 
-const dns = require('dns').promises;
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const urlRegex = require('./helpers/urlRegex');
-const generatePathStr = require('./helpers/generatePathStr');
-const shortURL = require('./models/shortURL');
+const {
+  checkValidURL,
+  generateShortURL,
+  saveShortURL,
+} = require('./middleware');
 
 const app = express();
 
@@ -44,51 +45,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// POST route to submit URLs to be shortened:
-app.post('/api/shorturl', (req, res) => {
-  const urlStr = req.body.url;
-
-  // Test given URL string against regex
-  const urlMatch = urlStr.match(urlRegex);
-  if (!urlMatch) {
-    return res.json({ error: 'invalid url', url: urlStr });
-  }
-
-  // If no PROTOCOL, then add one, get hostname from URL object:
-  const { PROTOCOL: protocol, URL: url } = urlMatch.groups;
-  const hostname = new URL(`${protocol || 'https://'}${url}`).hostname;
-
-  // Check hostname is valid / exists via dns
-  dns
-    .resolve(hostname)
-    .then(async () => {
-      // URL is legitimate, add shortened URL to DB and return
-      let tries = 0;
-      let short_url;
-
-      // Try up to 5 times to generate a unique, unused short url
-      while (tries < 5) {
-        short_url = generatePathStr();
-
-        const exists = await shortURL.findOne({ short_url });
-        if (!exists) {
-          return Promise.resolve(short_url);
-        }
-      }
-
-      // Something has gone wrong generating a unique short url
-      return Promise.reject('Could not find a unique, unused short url');
-    })
-    // Otherwise create a MongoDB Document and return info as JSON to user
-    .then((short_url) => shortURL.create({ original_url: urlStr, short_url }))
-    .then(({ original_url, short_url }) =>
-      res.json({ original_url, short_url }),
-    )
-    .catch((err) => {
-      console.error('ERROR when trying to resolve given address: ', err);
-      return res.json({ error: 'invalid url', url: urlStr });
-    });
-});
+// POST route to submit URLs to be shortened
+// Returns json with original and shortened url path if successful
+app.post(
+  '/api/shorturl',
+  checkValidURL,
+  generateShortURL,
+  saveShortURL,
+  (req, res) => {
+    const { original_url, short_url } = res.data.shortURLDocument;
+    return res.json({ original_url, short_url });
+  },
+);
 
 // 404 page not found:
 app.get('*', function (req, res) {
---
+++ middleware.js
@@ -0,0 +1,80 @@
+const dns = require('dns').promises;
+
+const urlRegex = require('./helpers/urlRegex');
+const generatePathStr = require('./helpers/generatePathStr');
+const ShortURL = require('./models/shortURL');
+
+const middleware = {};
+
+// Takes 'url' parameter from req.body and determines if url is valid:
+middleware.checkValidURL = (req, res, next) => {
+  const urlStr = req.body.url;
+
+  // Test given URL string against regex
+  const urlMatch = urlStr.match(urlRegex);
+  if (!urlMatch) {
+    return res.json({ error: 'invalid url', url: urlStr });
+  }
+
+  // If no PROTOCOL (e.g. http://), then add one, get hostname via URL:
+  const { PROTOCOL: protocol, URL: url } = urlMatch.groups;
+  const hostname = new URL(`${protocol || 'https://'}${url}`).hostname;
+
+  // Check hostname is valid / exists via dns
+  dns
+    .resolve(hostname)
+    .then(() => {
+      //If we get here url is valid, go to next middleware:
+      next();
+    })
+    .catch((err) => {
+      console.error('ERROR when trying to resolve given address: ', err);
+      return res.json({ error: 'invalid url', url: urlStr });
+    });
+};
+
+// Generate a random, unique short URL string
+// Add it to res.data.short_url
+middleware.generateShortURL = async (req, res, next) => {
+  res.data = res.data || {};
+  let tries = 0;
+
+  // Try up to 5 times to generate a unique, unused short url
+  while (tries < 5) {
+    const short_url = generatePathStr();
+    try {
+      const exists = await ShortURL.findOne({ short_url });
+      if (!exists) {
+        res.data.short_url = short_url;
+        return next();
+      }
+    } catch (err) {
+      console.error(
+        'Error when checking if short_url exists in database: ',
+        err,
+      );
+    }
+    tries += 1;
+  }
+
+  // If we get here, generating the unique short URL has failed:
+  return res.json({ error: 'Could not generate a unique short URL' });
+};
+
+// Given the unique shortURL on res.data.short_url, create a new ShortURL document
+// Add the ShortURL document to res.data.shortURLDocument
+middleware.saveShortURL = (req, res, next) => {
+  const original_url = req.body.url;
+  const short_url = res.data.short_url;
+  ShortURL.create({ original_url, short_url })
+    .then((document) => {
+      res.data.shortURLDocument = document;
+      next();
+    })
+    .catch((err) => {
+      console.error('Error when trying to create ShortURL in DB: ', err);
+      return res.json({ error: 'Could not save ShortURL in DB' });
+    });
+};
+
+module.exports = middleware;
